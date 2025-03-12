0ae3f024f73765a5a4e4b10ab4a82a79eaa2980b
matt n <mathiasnitz@gmx.de>
2025-03-12 11:56:20 +0000

    2nd try 5th step

--- myApp.js
+++ myApp.js
@@ -50,10 +50,7 @@ const createManyPeople = (arrayOfPeople, done) => {
 }
 
 const findPeopleByName = (personName, done) => {
-  
-  Person.find({ name: personName }, (data) => {
-    done(null, data);
-  })
+  Person.find({ name: personName }, (err, data) => done(err,data));
 };
 
 const findOneByFood = (food, done) => {
