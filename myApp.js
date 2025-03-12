8cb54d2a8e925c9a0945db0bbf5e8cfd024233f7
matt n <mathiasnitz@gmx.de>
2025-03-12 12:29:31 +0000

    2nd try 12th step

--- myApp.js
+++ myApp.js
@@ -104,7 +104,7 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  Person.find({ favoriteFoods: foodToSearch}).sort({name: 1}).limit(2).select('age').exec((err, data) => done(err,data));
+  Person.find({ favoriteFoods: foodToSearch}).sort({name: 1}).limit(2).select('-age').exec((err, data) => done(err,data));
 };
 
 
