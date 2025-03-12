c9a975889ff470734b00f9d4d53f49b4cd2aec9b
matt n <mathiasnitz@gmx.de>
2025-03-12 10:40:12 +0000

    4thtry 1step

--- myApp.js
+++ myApp.js
@@ -1,12 +1,16 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 
 
 
 let Person;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+});
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
