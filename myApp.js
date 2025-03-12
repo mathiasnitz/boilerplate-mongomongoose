cd267b0a943a5b0179507a413b4ec5d23af1bc9e
matt n <mathiasnitz@gmx.de>
2025-03-12 10:56:14 +0000

    7thtry 1st step

--- myApp.js
+++ myApp.js
@@ -7,10 +7,7 @@ const mongoose = require('mongoose');
 
 let Person;
 
-mongoose.connect(process.env.MONGO_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
-});
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
