de8e42ac3fbeb6b78743b3e2b7cdfebe2980cb99
matt n <mathiasnitz@gmx.de>
2025-03-12 11:02:37 +0000

    9thtry 1step

--- myApp.js
+++ myApp.js
@@ -7,7 +7,12 @@ const mongoose = require('mongoose');
 
 let Person;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("✅ Erfolgreich mit MongoDB verbunden!"))
+.catch(err => console.error("❌ Fehler beim Verbinden mit MongoDB:", err));
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
